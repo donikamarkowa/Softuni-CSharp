513b89735922a07ad9c25e934d9235637bbf710d
donikamarkowa <donimarkova@gmail.com>
2023-02-01 21:10:19 +0200

    Add ex.3

---
+++ C#DB/MS SQL/05.Subqueries and Joins/03.SalesEmployees.sql
@@ -0,0 +1,10 @@
+SELECT 
+	[e].[EmployeeID],
+	[e].[FirstName],
+	[e].[LastName],
+	[d].[Name] AS [DepartmentName]
+FROM [Employees] AS [e]
+LEFT JOIN [Departments] AS [d]
+ON [e].[DepartmentID] = [d].[DepartmentID]
+WHERE [d].[Name] = 'Sales'
+ORDER BY [EmployeeID]
