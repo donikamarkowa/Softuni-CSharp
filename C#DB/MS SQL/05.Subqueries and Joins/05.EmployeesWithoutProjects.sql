6535de9c64ca867b99b51a92f6e6a265936dc3d3
donikamarkowa <donimarkova@gmail.com>
2023-02-02 11:23:02 +0200

    Add ex.5

---
+++ C#DB/MS SQL/05.Subqueries and Joins/05.EmployeesWithoutProjects.sql
@@ -0,0 +1,8 @@
+SELECT TOP(3)
+	[e].[EmployeeID],
+	[e].[FirstName]
+FROM [Employees] AS [e]
+LEFT JOIN [EmployeesProjects] AS [ep]
+ON [e].[EmployeeID] = [ep].[EmployeeID]
+WHERE [ep].EmployeeID IS NULL
+ORDER By [e].[EmployeeID]
