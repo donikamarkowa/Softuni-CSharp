5d83eb81529adb8cf79718dd94ea0feb56ccdc1e
donikamarkowa <donimarkova@gmail.com>
2023-02-02 11:36:03 +0200

    Add ex.7

---
+++ C#DB/MS SQL/05.Subqueries and Joins/07.EmployeesWithProject.sql
@@ -0,0 +1,11 @@
+SELECT TOP(5)
+	[e].[EmployeeID],
+	[e].[FirstName],
+	[p].[Name] AS [ProjectName]
+FROM [Employees] AS [e]
+LEFT JOIN [EmployeesProjects] AS [ep]
+ON [e].[EmployeeID] = [ep].EmployeeID
+LEFT JOIN [Projects] AS [p]
+ON [p].[ProjectID] = [ep]. [ProjectID]
+WHERE [p].[StartDate] > '08/13/2002' 
+AND [p].[EndDate] IS NULL
