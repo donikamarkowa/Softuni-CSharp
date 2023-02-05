e9ed65a6f4798345a64309fa0f0e0b93ec8482a9
donikamarkowa <donimarkova@gmail.com>
2023-02-05 09:50:40 +0200

    Add ex.2

---
+++ C#DB/MS SQL/07.Functions and Stored Procedures/02.EmployeesWithSalaryAboveNumber.sql
@@ -0,0 +1,11 @@
+CREATE PROC usp_GetEmployeesSalaryAboveNumber(@Number DECIMAL(18,4))
+AS
+SELECT 
+	[FirstName] AS [First Name],
+	[LastName] AS [Last Name]
+FROM [Employees]
+WHERE [Salary] >= @Number
+
+GO
+
+EXEC usp_GetEmployeesSalaryAboveNumber 48100
