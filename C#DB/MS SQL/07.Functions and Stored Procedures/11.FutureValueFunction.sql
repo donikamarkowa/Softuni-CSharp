0bb28b479ce78d1851ab296ce1db43dd9d23f12a
donikamarkowa <donimarkova@gmail.com>
2023-02-05 11:18:59 +0200

    Add ex.11

---
+++ C#DB/MS SQL/07.Functions and Stored Procedures/11.FutureValueFunction.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION ufn_CalculateFutureValue (@Sum DECIMAL(18,4), @Yrate FLOAT, @years INT)
+RETURNS DECIMAL(18,4)
+BEGIN
+    DECLARE @Result DECIMAL(18,4) = @Sum * POWER((1+ @Yrate),@years)
+    RETURN @Result
+END
+
+GO 
+
+SELECT dbo.ufn_CalculateFutureValue (1000, 0.1, 5)
