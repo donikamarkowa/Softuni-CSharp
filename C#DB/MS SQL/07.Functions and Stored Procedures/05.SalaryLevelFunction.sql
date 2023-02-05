10a43ddd801a27d109f6a41bba679d55001b1b12
donikamarkowa <donimarkova@gmail.com>
2023-02-05 10:22:48 +0200

    Add ex.5

---
+++ C#DB/MS SQL/07.Functions and Stored Procedures/05.SalaryLevelFunction.sql
@@ -0,0 +1,21 @@
+CREATE FUNCTION ufn_GetSalaryLevel(@Salary DECIMAL(18,4))
+RETURNS NVARCHAR(10)
+AS
+BEGIN
+DECLARE @SalaryLevel NVARCHAR(10)
+IF (@Salary < 30000)
+	SET @SalaryLevel = 'Low'
+ELSE IF(@Salary <= 50000)
+    SET @SalaryLevel = 'Average'
+ELSE
+	SET @SalaryLevel = 'High'
+RETURN @SalaryLevel
+END
+
+GO
+
+SELECT 
+	[Salary],
+	dbo.ufn_GetSalaryLevel([Salary]) AS [Salary Level]
+FROM [Employees]
+
