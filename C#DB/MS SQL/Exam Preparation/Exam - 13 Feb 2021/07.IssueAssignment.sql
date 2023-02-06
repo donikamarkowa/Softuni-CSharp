fcb7f83f87176d8963086ac680d4ca414a1fa233
donikamarkowa <donimarkova@gmail.com>
2023-02-06 12:13:06 +0200

    Add ex.7

---
+++ C#DB/MS SQL/Exam Preparation/Exam - 13 Feb 2021/07.IssueAssignment.sql
@@ -0,0 +1,8 @@
+SELECT 
+	[i].[Id],
+	CONCAT([u].[Username], ' : ', [i].[Title]) AS [IssueAssignee]
+FROM [Issues] AS [i]
+INNER JOIN  [Users] AS [u]
+ON [i].[AssigneeId] = [u].[Id]
+ORDER BY [i].[Id] DESC, [i].[AssigneeId]
+
