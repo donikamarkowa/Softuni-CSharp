31999eca25953a34eb55c8eb7db6a60109f1ef6d
donikamarkowa <donimarkova@gmail.com>
2023-02-08 11:27:36 +0200

    add task 9

---
+++ C#DB/MS SQL/Exam Preparation/Exam - 16 October 2021/09.ClientsWithZIPCodes.sql
@@ -0,0 +1,16 @@
+SELECT
+	CONCAT([c].[FirstName], ' ', [c].[LastName]) AS [FullName],
+	[a].[Country],
+	[a].[ZIP],
+	CONCAT('$', 
+			(	SELECT MAX([PriceForSingleCigar])		
+				FROM [Cigars] AS [cig]
+				INNER JOIN [ClientsCigars] AS [cc]
+				ON [cig].[Id] = [cc].[CigarId]
+				AND [cc].[ClientId] = [c].[Id])
+			) AS [CigarPrice]
+FROM [Clients] AS [c]
+INNER JOIN [Addresses] AS [a] ON [a].[Id] = [c].[AddressId]
+WHERE ISNUMERIC([a].[ZIP]) = 1
+ORDER BY [FullName]
+
