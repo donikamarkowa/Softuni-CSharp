d5ce56b83a77f1d26560f49c9c01b259defc81de
donikamarkowa <donimarkova@gmail.com>
2023-02-15 19:09:49 +0200

    Add 06. Current Clients

---
+++ C#DB/MS SQL/Exam Preparation/Exam - 27 Jun 2020/06.CurrentClients.sql
@@ -0,0 +1,8 @@
+SELECT 
+	CONCAT([c].[FirstName], ' ', [c].[LastName]) AS [Client],
+	DATEDIFF(DAY, [j].[IssueDate], '2017-04-24') AS [Days going],
+	[j].[Status]
+FROM [Clients] AS [c]
+JOIN [Jobs] AS [j]
+ON [j].[ClientId] = [c].[ClientId]
+WHERE [j].[Status] <> 'Finished'
