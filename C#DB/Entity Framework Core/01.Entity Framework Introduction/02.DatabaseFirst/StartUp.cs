7e8312faef57df1199925ed72422072e540ca54e
donikamarkowa <donimarkova@gmail.com>
2023-03-03 20:16:56 +0200

    add 02. Database First

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/02.DatabaseFirst/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace _02.DatabaseFirst
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
