e5b03e04b7e398e652d8896584b444fe0e5408e9
donikamarkowa <donimarkova@gmail.com>
2023-03-05 13:07:38 +0200

    add 09. Employee 147

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/09.Employee147/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using SoftUni.Data;
+using System.Text;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployee147(context);
+
+            Console.WriteLine(result);
+        }
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employeeWithId147 = context
+                .Employees
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    Projects = e.Projects
+                    .Select(p => new
+                    {
+                        p.Name
+                    })
+                    .OrderBy(p => p.Name)   
+                    .ToList()
+                })
+                .ToList();
+
+            foreach (var e in employeeWithId147)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+
+                foreach (var p in e.Projects)
+                {
+                    sb.AppendLine(p.Name);
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
