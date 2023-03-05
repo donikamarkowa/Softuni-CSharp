71ca2eb439a570ced08d59eec9284c2cf0d58a8a
donikamarkowa <donimarkova@gmail.com>
2023-03-05 10:53:07 +0200

    06. Adding a New Address and Updating Employee

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/06.AddingANewAddressAndUpdatingEmployee/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SoftUniContext context = new SoftUniContext();    
+
+            string result = AddNewAddressToEmployee(context);
+
+            Console.WriteLine(result);
+        }
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            Address newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            Employee employee = context.Employees.FirstOrDefault(e => e.LastName == "Nakov");
+            employee.Address = newAddress;
+            context.SaveChanges();
+
+            var employeesWithTheirAddresses = context
+                .Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => e.Address.AddressText)
+                .ToList();
+
+            foreach (var e in employeesWithTheirAddresses)
+            {
+                sb.AppendLine(e);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
