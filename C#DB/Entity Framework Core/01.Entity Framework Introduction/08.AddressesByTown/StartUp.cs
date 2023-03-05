9dc3c0ea748ba2900e6259c0f7e69cd1d683f198
donikamarkowa <donimarkova@gmail.com>
2023-03-05 12:05:05 +0200

    Add 08. Addresses by Town

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/08.AddressesByTown/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           using SoftUniContext context = new SoftUniContext();
+
+            string result = GetAddressesByTown(context);
+
+            Console.WriteLine(result);
+        }
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder(); 
+
+            var addresses = context
+                .Addresses
+                .Select(a => new
+                {
+                    a.AddressText,
+                    TownName = a.Town.Name,
+                    EmployeesCount = a.Employees.Count()
+                })
+                .OrderByDescending(a => a.EmployeesCount)
+                .ThenBy(a => a.TownName)
+                .ThenBy(a => a.AddressText)
+                                .Take(10)
+                .ToList();
+
+            foreach (var a in addresses)
+            {
+                sb.AppendLine($"{a.AddressText}, {a.TownName} - {a.EmployeesCount} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
