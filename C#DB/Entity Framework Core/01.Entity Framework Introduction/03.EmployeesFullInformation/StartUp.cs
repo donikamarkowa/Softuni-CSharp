b91717285ed09b248e3aaf5d07bbe77cdbd5f7f8
donikamarkowa <donimarkova@gmail.com>
2023-03-05 09:49:15 +0200

    add ex.03

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployeesFullInformation(context);
+
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var allEmployees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.MiddleName,
+                    e.LastName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToList();
+
+            foreach (var employee in allEmployees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            return sb.ToString();
+        }
+    }
+}
