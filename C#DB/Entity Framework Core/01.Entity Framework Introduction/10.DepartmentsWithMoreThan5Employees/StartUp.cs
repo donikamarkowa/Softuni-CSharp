9d0034b93a9d837d972b05949d0a145a59b1da3f
donikamarkowa <donimarkova@gmail.com>
2023-03-08 10:56:20 +0200

    Add 10. Departments with More Than 5 Employees

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/10.DepartmentsWithMoreThan5Employees/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            string result = GetDepartmentsWithMoreThan5Employees(context);
+
+            Console.WriteLine(result);
+        }
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var departments = context
+                .Departments
+                .Where(d => d.Employees.Count > 5)
+                .Select(d => new
+                {
+                    d.Name,
+                    d.Manager,
+                    d.Employees
+                })
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .ToList();
+
+            foreach (var d in departments)
+            {
+                sb.AppendLine($"{d.Name} - {d.Manager.FirstName} {d.Manager.LastName}");
+                foreach (var e in d.Employees)
+                {
+                    sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+    }
+}
