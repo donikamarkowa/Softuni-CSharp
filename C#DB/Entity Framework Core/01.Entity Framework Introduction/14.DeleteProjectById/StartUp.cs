db61258ec2a0d6085619fafe4e8c38e4d5cee0af
donikamarkowa <donimarkova@gmail.com>
2023-03-08 12:18:18 +0200

    Add 15. Remove Town

--- C#DB/Entity Framework Core/01.Entity Framework Introduction/14.DeleteProjectById/StartUp.cs
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/14.DeleteProjectById/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using SoftUni.Data;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace SoftUni
 {
@@ -6,14 +8,39 @@ namespace SoftUni
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SoftUniContext softUniContext = new SoftUniContext();
+
+            string result = DeleteProjectById(softUniContext);
+
+            Console.WriteLine(result);
         }
         public static string DeleteProjectById(SoftUniContext context)
         {
+            StringBuilder sb = new StringBuilder(); 
+
             var projectsToDelete = context.Projects.Find(2);
 
             var projectsWithId2 = context
-                .EmployeesProject
+                .EmployeesProjects
+                .Where(p => p.ProjectId == projectsToDelete.ProjectId)
+                .ToList();
+
+            context.EmployeesProjects.RemoveRange(projectsWithId2);
+            context.Projects.Remove(projectsToDelete);
+            context.SaveChanges();
+
+            var projects = context
+                .Projects
+                .Take(10)
+                .Select(p => p.Name)
+                .ToList();
+
+            foreach (var p in projects)
+            {
+                sb.AppendLine(p);
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
