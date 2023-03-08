51a204a143eb2afac7ccaf1cbedd4468f3fddbb1
donikamarkowa <donimarkova@gmail.com>
2023-03-08 11:15:24 +0200

    Add 11. Find Latest 10 Projects

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/11.FindLatest10Projects/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            string result = GetLatestProjects(context);
+
+            Console.WriteLine(result);
+        }
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var projects = context
+                .Projects
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .OrderBy(p => p.Name)
+                .Select(p => new
+                {
+                    p.Name,
+                    p.StartDate,
+                    p.Description
+                })
+                .ToList();
+            foreach (var p in projects)
+            {
+                sb.AppendLine(p.Name);
+                sb.AppendLine(p.Description);
+                sb.AppendLine(p.StartDate.ToString("M/d/yyyy h:mm:ss tt"));
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
