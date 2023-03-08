d82f7002206f1e9c120380fc4aaa5f343f7747b7
donikamarkowa <donimarkova@gmail.com>
2023-03-08 11:30:59 +0200

    Add 12. Increase Salaries

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/12.IncreaseSalaries/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            string result = IncreaseSalaries(context);
+
+            Console.WriteLine(result);
+        }
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context
+                .Employees
+                .Where(e => e.Department.Name == "Engineering"
+                         || e.Department.Name == "Tool Design"
+                         || e.Department.Name == "Marketing"
+                         || e.Department.Name == "Information Services")
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                e.Salary += e.Salary*(decimal)0.12;
+                context.SaveChanges();
+                sb.AppendLine($"{e.FirstName} {e.LastName} (${e.Salary:F2})");
+            }
+            
+            return sb.ToString().TrimEnd(); 
+        }
+    }
+}
