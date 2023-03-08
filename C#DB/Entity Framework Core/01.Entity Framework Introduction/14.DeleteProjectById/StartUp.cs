83c8c6dddd411b4cebbf5e362f184f7af0573576
donikamarkowa <donimarkova@gmail.com>
2023-03-08 12:01:03 +0200

    Add 14. Delete Project by Id

---
+++ C#DB/Entity Framework Core/01.Entity Framework Introduction/14.DeleteProjectById/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            var projectsToDelete = context.Projects.Find(2);
+
+            var projectsWithId2 = context
+                .EmployeesProject
+        }
+    }
+}
