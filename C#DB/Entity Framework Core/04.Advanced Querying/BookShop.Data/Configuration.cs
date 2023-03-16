51610f49f2dd794209f5a6256eb89daf5eb0dddf
donikamarkowa <donimarkova@gmail.com>
2023-03-16 11:12:15 +0200

    Add 02. Age Restriction

---
+++ C#DB/Entity Framework Core/04.Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => @"Server=DESKTOP-CTSCET9\SQLEXPRESS01;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ C#DB/Entity Framework Core/04.Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.ComponentModel.DataAnnotations;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string ageRestriction = Console.ReadLine();
+            string result = GetBooksByAgeRestriction(db, ageRestriction);
+            Console.WriteLine(result);
+        }
+
+        //Problem 02
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            try
+            {
+                AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+                var bookTitles =  context.Books
+                    .Where(b => b.AgeRestriction == ageRestriction)
+                    .Select(b => b.Title)
+                    .OrderBy(t => t)
+                    .ToArray();
+
+                return string.Join(Environment.NewLine, bookTitles);
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+    }
+}
+
+
