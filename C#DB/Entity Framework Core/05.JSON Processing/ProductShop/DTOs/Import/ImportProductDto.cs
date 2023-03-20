460964258c759d5f436404d1c2efbd0f2151ef39
donikamarkowa <donimarkova@gmail.com>
2023-03-20 17:42:54 +0200

    Add 02. Import Products

--- C#DB/Entity Framework Core/05.JSON Processing/ProductShop/DTOs/Import/ImportProductDto.cs
+++ C#DB/Entity Framework Core/05.JSON Processing/ProductShop/DTOs/Import/ImportProductDto.cs
@@ -8,10 +8,13 @@ namespace ProductShop.DTOs.Import
     {
         [JsonProperty("Name")]
         public string Name { get; set; } = null!;
+
         [JsonProperty("Price")]
         public decimal Price { get; set; }
+
         [JsonProperty("SellerId")]
         public int SellerId { get; set; }
+
         [JsonProperty("BuyerId")]
         public int? BuyerId { get; set; }
     }
