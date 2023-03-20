85b1d1a7e597f0ec44da823b189b07a11f59d8be
donikamarkowa <donimarkova@gmail.com>
2023-03-20 16:40:16 +0200

    Add 01. Import Users

---
+++ C#DB/Entity Framework Core/05.JSON Processing/ProductShop/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+
+            string inputJson = File.ReadAllText(@"../../../Datasets/users.json");
+            string result = ImportUsers(dbContext, inputJson);
+            Console.WriteLine(result);
+
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+
+            ImportUserDto[] userDtos = 
+                JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+
+            ICollection<User> users = new HashSet<User>();  
+
+            foreach (var userDto in userDtos)
+            {
+                User user = mapper.Map<User>(userDto);
+
+                users.Add(user);
+            }
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+    }
+}
