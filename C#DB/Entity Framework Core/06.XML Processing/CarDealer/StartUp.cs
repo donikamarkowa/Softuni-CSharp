2553564d104cde4ad619079a9a69c1c03576efb5
donikamarkowa <donimarkova@gmail.com>
2023-03-23 18:24:29 +0200

    Add 09. Import Suppliers

---
+++ C#DB/Entity Framework Core/06.XML Processing/CarDealer/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using CarDealer.Utilities;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string inputXml = File.ReadAllText(@"../../../Datasets/suppliers.xml");
+            string result = ImportSuppliers(context, inputXml);
+            Console.WriteLine(result);
+        }
+        //Problem 01
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+            
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers");
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(ImportSupplierDto[]), xmlRoot);
+
+            StringReader reader = new StringReader(inputXml);
+            ImportSupplierDto[] supplierDtos = (ImportSupplierDto[])xmlSerializer.Deserialize(reader);
+
+            // XmlHelper xmlHelper = new XmlHelper();
+            // ImportSupplierDto[] supplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            ICollection<Supplier> suppliers = new HashSet<Supplier>();
+            foreach (var supplierDto in supplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue;
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+                suppliers.Add(supplier);
+            }
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}";
+        }
+    }
+}
---
+++ C#DB/Entity Framework Core/06.XML Processing/CarDealer/Utilities/XmlHelper.cs
@@ -0,0 +1,30 @@
+﻿using CarDealer.DTOs.Import;
+using System.Collections;
+using System.Xml.Serialization;
+
+namespace CarDealer.Utilities
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            StringReader reader = new StringReader(inputXml);
+            T suppliersDto = (T)xmlSerializer.Deserialize(reader);
+
+            return suppliersDto;
+        }
+        public IEnumerable<T> DeserializeCollection<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRoot);
+
+            StringReader reader = new StringReader(inputXml);
+            T[] suppliersDto = (T[])xmlSerializer.Deserialize(reader);
+
+            return suppliersDto;
+        }
+    }
+}
