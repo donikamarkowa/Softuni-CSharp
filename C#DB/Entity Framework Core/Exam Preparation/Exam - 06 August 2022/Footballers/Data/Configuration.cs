4e9d88fe1b304f816743fb573a6c4fb1a9587ff3
donikamarkowa <donimarkova@gmail.com>
2023-03-27 15:09:05 +0300

    Add 01. Model Definition

---
+++ C#DB/Entity Framework Core/Exam Preparation/Exam - 06 August 2022/Footballers/Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace Footballers.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-CTSCET9\SQLEXPRESS01;Database=FootballersExam;Integrated Security = True;TrustServerCertificate=True;"
+;
+    }
+}
---
+++ C#DB/Entity Framework Core/Exam Preparation/Exam - 06 August 2022/Footballers/Data/Models/Coach.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Footballers.Data.Models
+{
+    public class Coach
+    {
+        public Coach()
+        {
+            this.Footballers = new HashSet<Footballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Nationality { get; set; } = null!;
+
+        [InverseProperty(nameof(Footballer.Coach))]
+        public ICollection<Footballer> Footballers { get; set; } = null!;
+    }
+}
