598c9a024f3020b784c3ff88013621f0ea1731f3
donikamarkowa <donimarkova@gmail.com>
2023-03-30 20:18:28 +0300

    Add 03. Data Export

--- C#DB/Entity Framework Core/Exam Preparation/Retake Exam - 16 Dec 2021/Artillery/DataProcessor/Serializer.cs
+++ C#DB/Entity Framework Core/Exam Preparation/Retake Exam - 16 Dec 2021/Artillery/DataProcessor/Serializer.cs
@@ -2,17 +2,78 @@
 namespace Artillery.DataProcessor
 {
     using Artillery.Data;
+    using Artillery.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using System.Text;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
         public static string ExportShells(ArtilleryContext context, double shellWeight)
         {
-            throw new NotImplementedException();
+            var shells = context
+                .Shells
+                .Where(s => s.ShellWeight > shellWeight)
+                .ToArray()
+                .Select(s => new
+                {
+                    ShellWeight = s.ShellWeight,
+                    Caliber = s.Caliber,
+                    Guns = s.Guns
+                    .Select(g => new
+                    {
+                        GunType = g.GunType.ToString(),
+                        GunWeight = g.GunWeight,
+                        BarrelLength = g.BarrelLength,
+                        Range = g.Range > 3000 ? "Long-range" : "Regular range"
+                    })
+                    .Where(g => g.GunType.ToString() == "AntiAircraftGun")
+                    .OrderByDescending(g => g.GunWeight)
+                    .ToArray()
+                })
+                .OrderBy(s => s.ShellWeight)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(shells, Formatting.Indented);
         }
 
         public static string ExportGuns(ArtilleryContext context, string manufacturer)
         {
-            throw new NotImplementedException();
+            var guns = context
+                .Guns
+                .Where(g => g.Manufacturer.ManufacturerName == manufacturer)
+                .ToArray()
+                .Select(g => new ExportGunDto()
+                {
+                    Manufacturer = g.Manufacturer.ManufacturerName,
+                    GunType = g.GunType.ToString(),
+                    BarrelLength = g.BarrelLength,
+                    GunWeight = g.GunWeight,
+                    Range = g.Range,
+                    Countries = g.CountriesGuns
+                        .Select(c => new ExportCountryDto()
+                        {
+                            Country = c.Country.CountryName,
+                            ArmySize = c.Country.ArmySize
+                        })
+                        .Where(c => c.ArmySize > 4500000)
+                        .OrderBy(c => c.ArmySize)
+                        .ToArray()
+
+                })
+                .OrderBy(g => g.BarrelLength)
+                .ToArray();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportGunDto[]), new XmlRootAttribute("Guns"));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, guns, xmlNamespaces);
+            return sb.ToString();
         }
     }
 }
