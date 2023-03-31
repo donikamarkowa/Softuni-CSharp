21b33a59195486ad3b2184c1ef125cb909735dbe
donikamarkowa <donimarkova@gmail.com>
2023-03-31 12:05:53 +0300

    Add 03. Data Export

--- C#DB/Entity Framework Core/Exam Preparation/Exam - 04 Dec 2021/Theatre/DataProcessor/Serializer.cs
+++ C#DB/Entity Framework Core/Exam Preparation/Exam - 04 Dec 2021/Theatre/DataProcessor/Serializer.cs
@@ -1,19 +1,80 @@
 ﻿namespace Theatre.DataProcessor
 {
-
+    using Newtonsoft.Json;
     using System;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
     using Theatre.Data;
+    using Theatre.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
         {
-            throw new NotImplementedException();
+            var theatres = context
+                .Theatres
+                .Where(t => t.NumberOfHalls >= numbersOfHalls && t.Tickets.Count >= 20)
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Halls = t.NumberOfHalls,
+                    TotalIncome = t.Tickets
+                        .Where(ti => ti.RowNumber >= 1 && ti.RowNumber <= 5)
+                        .Sum(ti => ti.Price),
+                    Tickets = t.Tickets.Select(t => new
+                    {
+                        Price = t.Price,
+                        RowNumber = t.RowNumber
+                    })
+                    .Where(t => t.RowNumber >= 1 && t.RowNumber <= 5)
+                    .OrderByDescending(t => t.Price)
+                    .ToArray()
+                })
+               .OrderByDescending(t => t.Halls)
+               .ThenBy(t => t.Name)
+               .ToArray();
+
+            return JsonConvert.SerializeObject(theatres, Formatting.Indented);
+
         }
 
         public static string ExportPlays(TheatreContext context, double raiting)
         {
-            throw new NotImplementedException();
+            var plays = context.Plays
+                .Where(p => p.Rating <= raiting)
+                .ToArray()
+                .Select(p => new ExportPlayDto()
+                {
+                    Title = p.Title,
+                    Duration = p.Duration.ToString("c"),
+                    Rating = p.Rating == 0 ? "Premier" : p.Rating.ToString(),
+                    Genre = p.Genre.ToString(),
+                    Actors = p.Casts
+                    .Where(c => c.IsMainCharacter)
+                        .Select(c => new ExportActorDto()
+                        {
+                            FullName = c.FullName,
+                            MainCharacter = $"Plays main character in '{p.Title}'."
+                        })
+                        .OrderByDescending(c => c.FullName)
+                        .ToArray()
+
+                })
+                .OrderBy(p => p.Title)
+                .ThenByDescending(p => p.Genre)
+                .ToArray();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportPlayDto[]), new XmlRootAttribute("Plays"));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, plays, xmlNamespaces);
+            return sb.ToString();
         }
     }
 }
