52192712aab20e74a952d719c93ef8b4e6260c60
donikamarkowa <donimarkova@gmail.com>
2023-03-31 18:43:35 +0300

    Add 03. Data Export

--- C#DB/Entity Framework Core/Exam Preparation/Exam - 04 April 2021/TeisterMask/DataProcessor/Serializer.cs
+++ C#DB/Entity Framework Core/Exam Preparation/Exam - 04 April 2021/TeisterMask/DataProcessor/Serializer.cs
@@ -1,17 +1,79 @@
 ﻿namespace TeisterMask.DataProcessor
 {
     using Data;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
+    using TeisterMask.Data.Models.Enums;
+    using TeisterMask.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
         {
-            throw new NotImplementedException();
+            var projects = context
+                .Projects
+                .Where(p => p.Tasks.Any())
+                .ToArray()
+                .Select(p => new ExportProjectDto()
+                {
+                    ProjectName = p.Name,
+                    TaskCount = p.Tasks.Count(),
+                    HasEndDate = p.DueDate == null ? "No" : "Yes",
+                    ExportedTasks = p.Tasks.Select(t => new ExportTaskDto()
+                    {
+                        Name = t.Name,
+                        Label = t.LabelType.ToString()
+                    })
+                    .OrderBy(t => t.Name)
+                    .ToArray()
+                })
+                .OrderByDescending(p => p.TaskCount)
+                .ThenBy(t => t.ProjectName)
+                .ToArray();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportProjectDto[]), new XmlRootAttribute("Projects"));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, projects, xmlNamespaces);
+            return sb.ToString();
         }
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var busiestEmployees = context
+                .Employees
+                .Where(e => e.EmployeesTasks.Any(et => et.Task.OpenDate >= date))
+                .ToArray()
+                .Select(e => new
+                {
+                    Username = e.Username,
+                    Tasks = e.EmployeesTasks
+                    .Where(et => et.Task.OpenDate >= date)
+                    .OrderByDescending(et => et.Task.DueDate)
+                    .ThenBy(et => et.Task.Name)
+                    .Select(et => new
+                    {
+                        TaskName = et.Task.Name,
+                        OpenDate = et.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                        DueDate = et.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                        LabelType = et.Task.LabelType.ToString(),
+                        ExecutionType = et.Task.ExecutionType.ToString()
+                    })
+                    .ToArray()
+                })
+                .OrderByDescending(e => e.Tasks.Count())
+                .ThenBy(e => e.Username)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(busiestEmployees, Formatting.Indented);
         }
     }
 }
