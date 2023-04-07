c88c7cbd27b85671673853173298a0e913d4b8ec
donikamarkowa <donimarkova@gmail.com>
2023-04-07 16:28:32 +0300

    add

---
+++ C#Basics/01.First Steps In Coding/Lab/01.HelloSoftUni/01.First Steps In Coding/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.First_Steps_In_Coding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
