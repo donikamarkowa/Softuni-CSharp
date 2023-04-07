6d981c270fb94c61a845c5e0080f72d5537295b3
donikamarkowa <donimarkova@gmail.com>
2023-04-07 16:58:04 +0300

    Add

--- C#Basics/02.Conditional Statements/Lab/Program.cs
+++ C#Basics/02.Conditional Statements/Lab/Program.cs
@@ -1,10 +1,120 @@
 ﻿namespace Lab
 {
-    internal class Program
+    using System;
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //01. Excellent Result
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+
+            //02. Greater Number
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            if (firstNum > secondNum)
+            {
+                Console.WriteLine(firstNum);
+            }
+            else
+            {
+                Console.WriteLine(secondNum);
+            }
+
+            //03. Even or Odd
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+            //04. Password Guess
+            string password = Console.ReadLine();
+            if (password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+
+            }
+
+            //05. Number 100...200
+            int number = int.Parse(Console.ReadLine());
+            if (number < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (number >= 100 && number <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else if (number > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+
+            //06. Speed Info
+            double speed = double.Parse(Console.ReadLine());
+            if (speed <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (speed > 10 && speed <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if (speed > 50 && speed <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (speed > 150 && speed <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else if (speed > 1000)
+            {
+                Console.WriteLine("extremely fast");
+            }
+
+            //07. Area of Figures
+            string figure = Console.ReadLine();
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double S = a * a;
+                Console.WriteLine($"{S:F3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double S = a * b;
+                Console.WriteLine($"{S:F3}");
+            }
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double S = r * r * Math.PI;
+                Console.WriteLine($"{S:F3}");
+            }
+            else if (figure == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                double S = (a * h) / 2;
+                Console.WriteLine($"{S:F3}");
+            }
+
         }
     }
 }
